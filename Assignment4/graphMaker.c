919cda0fe9995be2a42e659052c6ae086a9f677b
RylakRila <kylvark@gmail.com>
2024-03-13 21:17:21 -0400

    step 2 of A4

--- Assignment4/graphMaker.c
+++ Assignment4/graphMaker.c
@@ -19,15 +19,30 @@ unsigned char linesIntersect(short v1x, short v1y, short v2x, short v2y, short v
 	return 0;
 }
 
+int squareDistance(Vertex* v1, Vertex* v2) {
+	return (v2->x - v1->x)*(v2->x - v1->x) + (v2->y - v1->y)*(v2->y - v1->y);
+}
+
+int isIntersect(Vertex *vertex1, Vertex *vertex2, Environment *env) {
+	// go through each obstacle
+    for (int i = 0; i < env->numObstacles; i++) {
+        Obstacle obst = env->obstacles[i];
+        // check if each edge of the obstacle has intersect with path
+        if (linesIntersect(vertex1->x, vertex1->y, vertex2->x, vertex2->y, obst.x, obst.y, obst.x, obst.y - obst.h) ||
+			linesIntersect(vertex1->x, vertex1->y, vertex2->x, vertex2->y, obst.x, obst.y, obst.x + obst.w, obst.y) ||
+			linesIntersect(vertex1->x, vertex1->y, vertex2->x, vertex2->y, obst.x, obst.y - obst.h, obst.x + obst.w, obst.y - obst.h) ||
+            linesIntersect(vertex1->x, vertex1->y, vertex2->x, vertex2->y, obst.x + obst.w, obst.y, obst.x + obst.w, obst.y - obst.h)) {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
 int pointInObstacle(Environment *env, short x, short y);
+void findKNearNeighbor(Environment *env, Vertex* v);
 
 // Create a graph using the numVertices and k parameters of the given environment.
-void createGraph(Environment *env) {	
-
-	// ...
-	// WRITE YOUR CODE HERE
-	// ...
-	
+void createGraph(Environment *env) {
 	// Allocate memory for the vertices array
 	env->vertices = (Vertex **)malloc(env->numVertices * sizeof(Vertex *));
 	if (env->vertices == NULL) {
@@ -50,13 +65,17 @@ void createGraph(Environment *env) {
 		do {
 			insideObstacle = FALSE;
 			
-			env->vertices[i]->x = rand() % (env->maximumX);
-			env->vertices[i]->y = rand() % (env->maximumY);
+			env->vertices[i]->x = rand() % (env->maximumX + 1);
+			env->vertices[i]->y = rand() % (env->maximumY + 1);
 			
 			if (pointInObstacle(env, env->vertices[i]->x, env->vertices[i]->y)) 
 				insideObstacle = TRUE;
 			
 		} while (insideObstacle);
+	}
+	
+	for (int i = 0; i < env->numVertices; i++) {
+		findKNearNeighbor(env, env->vertices[i]);
 	}
 }
 
@@ -72,6 +91,76 @@ int pointInObstacle(Environment *env, short x, short y) {
 	return FALSE;
 }
 
+void findKNearNeighbor(Environment *env, Vertex* v) {
+	v->neighbours = (Neighbour *)malloc(sizeof(Neighbour));
+	if (v->neighbours == NULL) {
+		printf("Error: malloc failed to allocate memory for neighbours\n");
+		exit(-1);
+	}
+	
+	// Array to hold k nearest neighbours
+	Vertex** nearest = (Vertex **)malloc(env->k * sizeof(Vertex *));
+	if (nearest == NULL) {
+		printf("Error: malloc failed to allocate memory for nearest\n");
+		exit(-1);
+	}
+	int arraySize = 0;
+	
+	// go through each vertex except the vertex itself
+	for (int i = 0; i < env->numVertices; i++) {
+		if (v == env->vertices[i]) continue;
+		
+		Vertex* currentVertex = env->vertices[i];
+		if (isIntersect(v, currentVertex, env)) continue;
+		
+		int distance = squareDistance(v, currentVertex);
+		
+		if (arraySize < env->k) {
+			nearest[arraySize] = currentVertex;
+			arraySize++;
+			continue;
+		}
+		
+		// go through the nearest array and check 
+		// if the current vertex is closer to any vertex in the array
+		for (int j = 0; j < env->k; j++) {
+			if (distance < squareDistance(v, nearest[j])) {
+				// if it's closer, replace the farthest vertex with the current vertex
+				nearest[j] = currentVertex;
+				break;
+			}
+		}
+	}
+	
+	Neighbour* lastNbr = NULL;
+	// add the nearest vertices to the neighbours linked list
+	for (int i = 0; i < env->k; i++) {
+		Vertex *nbr = nearest[i];
+		
+		Neighbour *newNeighbour = (Neighbour *)malloc(sizeof(Neighbour));
+		if (newNeighbour == NULL) {
+			printf("Error: malloc failed to allocate memory for newNeighbour\n");
+			exit(-1);
+		}
+		
+		newNeighbour->vertex = nbr;
+		newNeighbour->next = NULL;
+		
+		// if it's the first neighbour, add it to the vertex's neighbours
+		if (lastNbr == NULL) {
+			v->neighbours = newNeighbour;
+		// if it's not the first neighbour, add it to the last neighbour's next
+		} else {
+			lastNbr->next = newNeighbour;
+		}
+		
+		// update lastNbr
+		lastNbr = newNeighbour;
+	}
+	
+	free(nearest);
+}
+
 // This procedure cleans up everything by creeing all alocated memory
 void cleanupEverything(Environment *env) {
 
@@ -80,5 +169,3 @@ void cleanupEverything(Environment *env) {
 	// ...
 	
 }
-
-
